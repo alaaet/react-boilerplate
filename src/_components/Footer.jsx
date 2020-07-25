ad430744108884f86590c8d16815a835f84007bc
alaaet <alaaet@gmail.com>
2020-07-26 00:08:52 +0200

    added dummy links to footer

--- src/_components/Footer.jsx
+++ src/_components/Footer.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-light top-shadow">
+    <nav
+      id="footer"
+      className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-light top-shadow"
+    >
       <p className="blocktext">
-        All rights reserved Reftag{" "}
-        <i className="fa fa-id-card" aria-hidden="true"></i>
+        <a href="#">Work with us</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
       </p>
     </nav>
   );
